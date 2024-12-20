37b4986c180f908c986f7f1117c993dcc1600387
TheJoboReal <kasper.jobo@gmail.com>
2024-12-20 09:56:19 +0100

    Added gamel eksamen

--- eksamen_1.cpp
+++ eksamen_1.cpp
@@ -18,12 +18,53 @@ int sumDeleligMedTreOgOtte(int N){
     return sum + sumDeleligMedTreOgOtte(N - 1);
 }
 
-bool sumAfToLigParameter(int arraylength, int l, int X){
-    std::array<int, 100> arr = {0};
 
+// Opgave 1 i reeksamen
+bool sumAfToLigParameter(int* arr, int l, int X){
+    arr = new int[l];
+
+    for(int i = 0; i < l; i++){
+        for(int k = 0; k < l; k++){
+            if(k != i){
+                if(k + i == X){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool isVokal(char k){
+    if(k == 'a'| k == 'e'| k == 'i'| k == 'o'| k == 'u'| k == 'y'){
+        return true;
+    }
+    return false;
+}
+
+// Opgave 3 i reeksamen
+int antalVokaler(std::string str, int l){
+    int vokaler = 0;
+    if(l < 0){
+        return 0;
+    }
+
+    if(isVokal(str[l - 1]) == 1){
+        vokaler++;
+    }
+
+    return vokaler + antalVokaler(str, l - 1);
+}
+
+// Opgave 4 
+void minSortering(int* arr){
+    
 }
 
 
 int main(){
-    std::cout << sumDeleligMedTreOgOtte(26) << std::endl;
+    // std::array<int, 5> arr = {1, 2, 3, 4, 5};
+    // std::cout << sumAfToLigParameter(arr.data(), 5, 4) << std::endl;
+    std::string str = "stationsbygninger";
+    std::cout << antalVokaler(str, str.size()) << std::endl;
 }
